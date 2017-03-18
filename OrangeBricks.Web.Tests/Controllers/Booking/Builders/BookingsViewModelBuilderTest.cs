5ecfb498032a52a04e4b955659406e924d8022ee
Ant <antoniomoore.am91@gmail.com>
2017-03-18 00:13:58 +0000

    Added unit tests
    > BookingsViewModelBuilderTest (Bookings On Property)
    > MyOffersViewModelBuilderTest (My Offers)

---
+++ OrangeBricks.Web.Tests/Controllers/Booking/Builders/BookingsViewModelBuilderTest.cs
@@ -0,0 +1,86 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using OrangeBricks.Web.Controllers.Bookings.Builders;
+using OrangeBricks.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace OrangeBricks.Web.Tests.Controllers.Booking.Builders
+{
+    public static class ExtentionMethods
+    {
+        public static IDbSet<T> Initialize<T>(this IDbSet<T> dbSet, IQueryable<T> data) where T : class
+        {
+            dbSet.Provider.Returns(data.Provider);
+            dbSet.Expression.Returns(data.Expression);
+            dbSet.ElementType.Returns(data.ElementType);
+            dbSet.GetEnumerator().Returns(data.GetEnumerator());
+            return dbSet;
+        }
+    }
+
+    [TestFixture]
+    public class BookingsViewModelBuilderTest
+    {
+        private IOrangeBricksContext _context;
+
+        [SetUp]
+        public void Setup()
+        {
+            _context = Substitute.For<IOrangeBricksContext>();
+        }
+
+        [Test]
+        public void BuildShouldReturnAllBookingsOnSinglePropertyWhenPropertyIdIsProvided()
+        {
+            // Arrange
+            var builder = new BookingsOnPropertyViewModelBuilder(_context);
+
+            var properties = new List<Models.Property>()
+            {
+                new Models.Property {
+                    Id = 1,
+                    Bookings = new List<Models.Booking>() {
+                        new Models.Booking
+                        {
+                            BookingDate = new DateTime(2017, 01, 01),
+                            Status = BookingStatus.Pending
+                        },
+                        new Models.Booking
+                        {
+                            BookingDate = new DateTime(2017, 01, 01),
+                            Status = BookingStatus.Pending
+                        }
+                    }
+                },
+                new Models.Property
+                {
+                    Id = 2,
+                    Bookings = new List<Models.Booking>()
+                    {
+                        new Models.Booking
+                        {
+                            BookingDate = new DateTime(2017, 01, 01),
+                            Status = BookingStatus.Pending
+                        }
+                    }
+                }
+            };
+
+            var mockSet = Substitute.For<IDbSet<Models.Property>>()
+                .Initialize(properties.AsQueryable());
+
+            _context.Properties.Returns(mockSet);
+
+            int propertyId = 1;
+
+            // Act
+            var viewModel = builder.Build(propertyId);
+
+            // Assert
+            Assert.That(viewModel.Bookings.Count, Is.EqualTo(2));
+        }
+    }
+}
---
+++ OrangeBricks.Web.Tests/Controllers/Offers/MyOffersViewModelBuilderTest.cs
@@ -0,0 +1,87 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using OrangeBricks.Web.Controllers.Offers.Builders;
+using OrangeBricks.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace OrangeBricks.Web.Tests.Controllers.Offers
+{
+    public static class ExtentionMethods
+    {
+        public static IDbSet<T> Initialize<T>(this IDbSet<T> dbSet, IQueryable<T> data) where T : class
+        {
+            dbSet.Provider.Returns(data.Provider);
+            dbSet.Expression.Returns(data.Expression);
+            dbSet.ElementType.Returns(data.ElementType);
+            dbSet.GetEnumerator().Returns(data.GetEnumerator());
+            return dbSet;
+        }
+    }
+
+    [TestFixture]
+    public class MyOffersViewModelBuilderTest
+    {
+        private IOrangeBricksContext _context;
+
+        [SetUp]
+        public void Setup()
+        {
+            _context = Substitute.For<IOrangeBricksContext>();
+        }
+
+        [Test]
+        public void BuilderShouldReturnAllOffersForBuyerWhenBuyerIdIsProvided()
+        {
+            // Arrange
+            var builder = new MyOffersViewModelBuilder(_context);
+
+            var BuyerUserIdOne = Guid.NewGuid().ToString();
+            var BuyerUserIdTwo = Guid.NewGuid().ToString();
+
+            var offers = new List<Offer>()
+            {
+                new Offer
+                {
+                    BuyerUserId = BuyerUserIdOne,
+                    Status = OfferStatus.Pending,
+                    Property = new Models.Property
+                    {
+                        Id = 1
+                    }
+                },
+                new Offer
+                {
+                    BuyerUserId = BuyerUserIdOne,
+                    Status = OfferStatus.Pending,
+                    Property = new Models.Property
+                    {
+                        Id = 2
+                    }
+                },
+                new Offer
+                {
+                    BuyerUserId = BuyerUserIdTwo,
+                    Status = OfferStatus.Pending,
+                    Property = new Models.Property
+                    {
+                        Id = 1
+                    }
+                }
+            };
+
+            var mockSet = Substitute.For<IDbSet<Offer>>()
+                .Initialize(offers.AsQueryable());
+
+            _context.Offers.Returns(mockSet);
+
+            // Act
+            var viewModel = builder.Build(BuyerUserIdOne);
+
+            // Assert
+            Assert.That(viewModel.Offers.Count, Is.EqualTo(2));
+        }
+    }
+}
