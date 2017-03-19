41d06e5e0df472d9cf884e4471e2fd8eee69b1ef
Ant <antoniomoore.am91@gmail.com>
2017-03-19 20:34:43 +0000

    Added ReactJS.NET
    Added dependencies for ReactJS.NET and a component for the MyOffers
    link.

---
+++ OrangeBricks.Web/Scripts/ViewComponents/MyOffers.jsx
@@ -0,0 +1,12 @@
+﻿var MyOffers = React.createClass({
+    getInitialState: function () {
+        return { data: this.props.intialData }
+    },
+    getBadge: function() {
+    },
+    render: function () {
+        return (
+            <a href={this.props.Url}>{this.props.Text}</a>
+        );
+    }
+});
